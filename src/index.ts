301c24584bfedf4ba4070f6339fe841ca10217a7
Akshar <mistry.aks@gmail.com>
2021-02-18 13:12:35 +0530

    Package updated

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ export * from '../errors/badRequestError';
 export * from '../errors/customError';
 export * from '../errors/dbConError';
 export * from '../errors/requestValidationError';
+export * from '../errors/notAuthorizedError';
 
 
 export * from '../middleware/errorHandler';
 export * from '../middleware/validationHandler';
+export * from '../middleware/currentUser';
+export * from '../middleware/requireAuth';
